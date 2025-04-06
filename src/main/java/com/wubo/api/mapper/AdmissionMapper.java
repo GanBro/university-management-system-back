ecd8c126de8a8dd062266e5d7ad83c47e6743f82
wubo <2551921037@qq.com>
2025-04-06 11:24:12 +0800

    refactor: 删除管理员日志相关代码
    - 删除了 AdminLog 实体类
    - 删除了 AdminLogMapper 接口
    - 删除了 AdminLogService 接口及其实现类
    - 删除了 AdmissionMapper接口

--- src/main/java/com/wubo/api/mapper/AdmissionMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wubo.api.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wubo.api.entity.AdminLog;
-
-public interface AdmissionMapper extends BaseMapper<AdminLog> {
-}
